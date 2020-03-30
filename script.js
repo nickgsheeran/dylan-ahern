fff55ab93a8f7b6027ef27e83fae4be7103780c6
Nick Sheeran <nickgsheeran@gmail.com>
2020-03-30 16:45:27 -0400

    window size check in script

--- script.js
+++ script.js
@@ -107,6 +107,11 @@ function contact_switch() {
 
 window.onload = function() {
     changeFont();
+    
+    document.body.height = window.innerHeight;
 }
 
+window.onresize = function () {
+    document.body.height = window.innerHeight;
+}
 get_data_array();
